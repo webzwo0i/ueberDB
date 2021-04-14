119aed3da183422009645f6e7286aee303e06546
webzwo0i <webzwo0i@c3d2.de>
2021-04-14 19:32:46 +0200

    first test attempt for timeouts

--- test/test_mysql.js
+++ test/test_mysql.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const Randexp = require('randexp');
+const randomString = (length = maxKeyLength) => new Randexp(new RegExp(`.{${length}}`)).gen();
 const assert = require('assert').strict;
 const {databases} = require('./lib/databases');
 const mysql = require('../databases/mysql_db');
@@ -28,5 +29,67 @@ describe(__filename, function () {
     const after = await db._connection;
     assert.notEqual(after, before);
     await db.close();
+  });
+
+  describe('timeouts', function () {
+    this.timeout(100000);
+    let large;
+    let largeKey;
+    let db;
+    const randomKey = randomString(10);
+
+    before(async function () {
+      db = new mysql.Database(databases.mysql);
+      await db.init();
+      large = randomString(10000);
+      largeKey = randomString(95);
+      await db.set(largeKey,large);
+      let i = 0;
+      // put some data in
+      while (i < 5000) {
+        await db.set(`${randomString(4)}${i}`, large);
+        i += 1;
+      }
+      db.settings.queryTimeout = 1;
+      db.logger = Object.setPrototypeOf({error() {}}, db.logger);
+    });
+
+    after(async function () {
+      db.close();
+    });
+
+    it('get times out ', async function () {
+      let errored = false;
+      for (let i=0; i < 10000; i++) {
+	try {
+          await db.findKeys('*');
+          await db.get(`${largeKey}${randomString(5)}`);
+	  // intentionally no break
+        } catch { errored = true; }
+	assert(errored);
+      }
+    });
+    it('set times out ', async function () {
+      let errored = false;
+      for (let i=0; i < 10000; i++) {
+	try {
+          await db.findKeys('*');
+          await db.set(randomString(10), large);
+	  // intentionally no break
+        } catch { errored = true; }
+	assert(errored);
+      }
+    });
+    it('findKeys times out ', async function () {
+      let errored = false;
+      for (let i=0; i < 10000; i++) {
+	try {
+          await db.findKeys('*');
+	  // intentionally no break
+        } catch { errored = true; }
+	assert(errored);
+      }
+      
+    });
   });
 });
